33b3241474e03437e5a92f272b3161e6654a12d0
pkumar07 <poornima.kumar@intel.com>
2019-09-10 16:35:51 -0700

    Added header and fixed css

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header'
 import OnnxValidateInput from './components/OnnxValidateInput'
 import OnnxDisplayResult from './components/OnnxDisplayResult'
+import './css/index.css';
 
 const App: React.SFC = () => {
 
     return (
-        <div>
-            <Header />
-            {/* <OnnxValidateInput /> */}
+        <div className="container">
+            <OnnxValidateInput />
             <OnnxDisplayResult />
         </div>
     )
